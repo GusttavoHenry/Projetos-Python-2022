d8e14cc42a6f3373084f07540138993d17d32a43
GusttavoHenry <gustavohgs244@gmail.com>
2022-10-20 17:50:31 -0300

    primeiro envio

---
+++ Atividade01/main.py
@@ -0,0 +1,32 @@
+# Primeiro projeto em python bora lá!!!!
+# print() = comando de saida
+
+print("Ola pessoas e mundo :)")
+
+#quando quiser armazenar uma string (frase)
+nome = "Gustavo Henrique"
+
+#quando quiser armazenar um inteiro
+idade = 19
+
+# exibir nome
+print(nome)
+
+# exibir idade
+print(idade)
+
+#quando quiser exibir uma frase "minha idade é" compleando com o uso de uma variavel
+print("minha idade é "+str(idade))
+print(f"minhaidade é {idade}\n")
+print("minha idade é {}".format(idade))
+
+#Quando quiser vexibi "meu nome é ... e tenho ... anos" utilizando mais de uma variavel
+print("meu nome é {} e tenho {}  anos".format(nome, idade))
+
+
+
+
+'''
+comentario em bloco 
+
+'''
